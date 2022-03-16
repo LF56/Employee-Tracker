5d0cff3d8ddc9a6ffdb4993154e7b6ff50f3d4a4
LF56 <leah.fox7@gmail.com>
2022-03-16 17:32:08 -0400

    added functions for console inmformation

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department
+INSERT INTO departments
     (name)
 VALUES
 ('Engineering'),
@@ -9,24 +9,25 @@ VALUES
 INSERT INTO roles
     (title, salary, department_id)
 VALUES
-('Sales Lead', 100000, ),
-('Sales Representative', 80000,),
-('Lead Engineer', 150000,),
-('Software Engineer', 120000),
-('Account Manager', 160000),
-('Legal Team Lead', 250000),
-('Lawyer', 190000, );
+('Sales Lead', 100000, 4),
+('Salesperson', 80000, 4),
+('Lead Engineer', 150000, 1),
+('Software Engineer', 120000, 1),
+('Account Manager', 160000, 2),
+('Accountant', 125000, 2),
+('Legal Team Lead', 250000, 3),
+('Lawyer', 190000, 3 );
 
-INSERT INTO employee
+INSERT INTO employees
     (first_name, last_name, role_id, manager_id)
 VALUES
-('Abby', 'Manuel', 1, 2),
+('Abby', 'Manuel', 1, NULL),
   ('Bill', 'Smith', 3, NULL ),
-  ('Charles', 'Gaveston', 4, 3 ),
-  ('Danelle', 'LeRoi', 3, NULL ),
-  ('Esther', 'Mansfield', 2, 5),
-  ('Fink', 'Carrington', 5, 2),
-  ('George', 'Bellamy', 6, NULL ),
-  ('Hannah', 'Summers',4,3 ),
-  ('Isabelle', 'Butler', 2, 5),
-  ('Josh', 'Steven',1, 2 );
+  ('Charles', 'Gaveston', 4,2 ),
+  ('Danelle', 'LeRoi',7, NULL),
+  ('Esther', 'Mansfield', 2, 1 ),
+  ('Fink', 'Carrington', 5, NULL),
+  ('George', 'Bellamy', 6,6  ),
+  ('Hannah', 'Summers',4, 2 ),
+  ('Isabelle', 'Butler', 2, 1 ),
+  ('Josh', 'Steven', 8, 4);
---
+++ index.js
@@ -0,0 +1,42 @@
+const table = require('console.table');
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+
+
+
+
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+});
+
+const viewAllEmployees = () => {
+  const sql = `SELECT * FROM employees`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      throw (err)
+    }
+    console.table(rows)
+    mainMenu()
+  });
+}
+
+const mainMenu = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to view?',
+      choices: ['View All Employees', 'View All Roles', 'View All Departments']
+    }
+  ])
+  .then(data => {
+    if (data.choice === 'View All Employees'){
+      viewAllEmployees()
+    }
+  })
+}
+
+mainMenu()
